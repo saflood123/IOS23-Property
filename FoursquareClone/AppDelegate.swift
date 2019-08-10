e5f1981d2492cfaae57d6d8bc14f3f1aa9222785
saflood123 <stuartflood@gmail.com>
2019-08-10 22:44:01 +0300

    first commit

--- FoursquareClone/AppDelegate.swift
+++ FoursquareClone/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ParseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let configuration = ParseClientConfiguration { (ParseMutableClientConfiguration) in
+            ParseMutableClientConfiguration.applicationId = "c0DmaMlujldjeJNeuj8bNCbk3FHnk33cEhuXhMPj"
+            ParseMutableClientConfiguration.clientKey = "3J7f3t0Q9Bny8Sf14gBcEICJPqwnKHiy9G2KiSgL"
+            ParseMutableClientConfiguration.server = "https://parseapi.back4app.com/"
+            
+        }
+        
+        Parse.initialize(with: configuration)
+        
+        
         return true
     }
 
--- FoursquareClone/SignUpVC.swift
+++ FoursquareClone/SignUpVC.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import Parse
+import ParseCore
 
 class SignUpVC: UIViewController {
 
